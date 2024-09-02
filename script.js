2593e21bd233b64e4741a366258779dc331def18
Ayeshhanif12 <ayeshahanif.0317@gmail.com>
2024-09-02 17:34:51 +0500

    rr

--- script.js
+++ script.js
@@ -39,6 +39,7 @@ function createGameElement(tag, className) {
 }
 // set the position of snake and food
 function setPosition(element, position) {
+
     console.log('Setting position:', position);
     element.style.gridColumn = position.x;
     element.style.gridRow = position.y;
@@ -49,7 +50,6 @@ function setPosition(element, position) {
 //draw food function 
 function drawFood() {
     if (gameStarted) {
-
         console.log('Drawing food at:', food);
         const foodElement = createGameElement('div', 'food');
         setPosition(foodElement, food);
